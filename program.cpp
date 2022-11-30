4a481a5614566cfdc393aa1e4bfe74d7840e9fb1
MrEnotEnotovich <kirill123456789094@gmail.com>
2022-11-30 23:17:26 +0300

    add function MyMul()

--- program.cpp
+++ program.cpp
@@ -1,18 +1,13 @@
 //файл program.cpp
 #include iostream
+#include "funcs.h"
 using namespace std;
-double MySum(double a, double b)
-{
-    return a+b;
-}
-double MySub(double a, double b)
-{
-    return a - b;
-}
+
 int main()
 {
 cout << MySum(10, 3) << endl;
 cout << MySub(10, 3) << endl;
+cout << MyMul(10, 3) << endl; 
 system("pause");
 return 0;
 }
